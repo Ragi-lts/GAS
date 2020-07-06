310f4967f0851775cba744bcf374216aa78ce31a
Ragi-lys <yuusan1694301@gmail.com>
2020-07-06 11:25:21 +0900

    master

--- src/SpreadSheet.js
+++
@@ -1,9 +0,0 @@
-function LoadDataBase (LINEAPI,DATABASEKEY=null){
-    var key = DATABASEKEY;
-    if (!SpreadsheetApp.openById(DATABASEKEY)) 
-    {
-        key =  SpreadsheetApp.create("DATABASE").getId();
-    }
-    return key;
-}
-
