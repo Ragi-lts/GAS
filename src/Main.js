493c5093202aeb370a4280b841a0cc796e6f614a
Ragi-lys <yuusan1694301@gmail.com>
2020-07-06 23:37:31 +0900

    master

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,24 @@
-
 //受信したら動作する
 //function doPost(e){
-function test(){  
-  const ACCESS_TOKEN = PropertiesService.getScriptProperties().getProperty('CHANNNELACCESSTOKEN');
-  const SECRET = PropertiesService.getScriptProperties().getProperty('CHANNNEL_SECRET'); 
-  const DATABASE = PropertiesService.getScriptProperties().getProperty('DATABASEKEY');
-  const BASE_URL = 'https://api.line.me/v2/bot';
-  const Zoom_BaseURL = "https://api.zoom.us/v2";
-  //var replyToken = JSON.parse(e.postData.contents).events[0].replyToken;
-  //var message = JSON.parse(e.postData.contents).events[0].message.text;
-  
- // const line_api = new LINEAPI(BASE_URL,ACCESS_TOKEN);
 
-  let Zoom_api = new Zoom(Zoom_BaseURL);
-  var Date1  = new Date();
-  Zoom_api.CreateMeeting("any", Date1);
+function test(){    //LocalTest
+
+  const ACCESS_TOKEN  = PropertiesService.getScriptProperties().getProperty('CHANNNELACCESSTOKEN');
+  const SECRET        = PropertiesService.getScriptProperties().getProperty('CHANNNEL_SECRET'); 
+  const DATABASE      = PropertiesService.getScriptProperties().getProperty('DATABASEKEY');
+  const BASE_URL      = 'https://api.line.me/v2/bot';
+  const Zoom_BaseURL  = "https://api.zoom.us/v2";
+  //var replyToken    = JSON.parse(e.postData.contents).events[0].replyToken;
+  //var message       = JSON.parse(e.postData.contents).events[0].message.text;
+  
+  //const  line_api = new LINEAPI(BASE_URL,ACCESS_TOKEN); /*LINEAPI操作用インスタンス作成*/
+  //const  zoom_api = new Zoom(Zoom_BaseURL);             /*Zoomの操作用インスタンス作成*/
 
 }
 
+//送信したら動作する
+/*
+function doGet(e){
+
+}
+*/
